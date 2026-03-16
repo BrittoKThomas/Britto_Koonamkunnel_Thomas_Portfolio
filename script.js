385e9813cb75005456f096136f1b8b92139e8c58
BrittoKThomas <brittothomas28@gmail.com>
2026-03-16 14:24:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ behavior:"smooth"
 
 }
 
-/* Smooth navigation scroll */
+/* smooth navigation */
 
 document.querySelectorAll("nav a").forEach(link => {
 
@@ -21,3 +21,50 @@ behavior:"smooth"
 })
 
 })
+
+/* typing animation */
+
+const text = [
+"QA Engineer",
+"Selenium Automation Engineer",
+"Java Test Automation Developer",
+"Software Tester"
+]
+
+let count = 0
+let index = 0
+let currentText = ""
+let letter = ""
+
+function type(){
+
+if(count === text.length){
+count = 0
+}
+
+currentText = text[count]
+
+letter = currentText.slice(0, ++index)
+
+document.querySelector(".typing").textContent = letter
+
+if(letter.length === currentText.length){
+count++
+index = 0
+setTimeout(type,1500)
+}
+else{
+setTimeout(type,100)
+}
+
+}
+
+type()
+
+/* dark mode toggle */
+
+document.getElementById("darkToggle").onclick = function(){
+
+document.body.classList.toggle("dark-mode")
+
+}
